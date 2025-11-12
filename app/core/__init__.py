e143629d84c797c72ab41f8e7cc2bfd9bfd5eaae
Stepan <bolshakov.ca@gmail.com>
2025-11-12 20:02:10 +0700

    Added core inits

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,3 +1,16 @@
 __all__ = ("settings",)
+__all__ = (
+    "settings",
+    "broker",
+    "emails_publisher",
+    "S3Client",
+    "db_manager",
+    "cache_storage",
+    "sessions_storage",
+)
 
 from .config import settings
+from .fs_broker import broker, emails_publisher
+from .s3_client import S3Client
+from .db_manager import db_manager
+from .redis_manager import cache_storage, sessions_storage
