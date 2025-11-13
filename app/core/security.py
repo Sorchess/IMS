812468ed630b3332bf2cbaced84e570eac56248f
Stepan <bolshakov.ca@gmail.com>
2025-11-13 12:51:29 +0700

    Added hash password methods

--- app/core/security.py
+++ app/core/security.py
@@ -1,5 +1,23 @@
 import uuid
+import bcrypt
 
 
 def generate_uuid() -> str:
     return uuid.uuid4().hex
+
+
+def hash_password(
+    password: str,
+) -> str:
+    salt = bcrypt.gensalt()
+    pw_bytes = password.encode()
+    return bcrypt.hashpw(pw_bytes, salt).decode("utf-8")
+
+
+def verify_password(
+    password: str,
+    hashed_password: str,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(), hashed_password=hashed_password.encode()
+    )
