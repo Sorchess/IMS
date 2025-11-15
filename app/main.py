6ab7de1287235b49cd4883b7cb0e309725770a57
Stepan <bolshakov.ca@gmail.com>
2025-11-15 08:30:39 +0700

    Added cors middleware

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ import uvicorn
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import api_router
 from app.core import settings, broker
@@ -32,6 +33,16 @@ app = FastAPI(
     title="IPC Monitoring",
     lifespan=lifespan,
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
 
 
 if __name__ == "__main__":
